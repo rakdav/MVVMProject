50b0f7cef7f84298f03adaa63c208177e3b28c41
Gogs <gogs@fake.local>
2024-05-13 11:13:59 +0200

    site

--- app/src/main/java/com/example/mvvmproject/model/ButtonData.kt
+++ app/src/main/java/com/example/mvvmproject/model/ButtonData.kt
@@ -1,5 +1,5 @@
 package com.example.mvvmproject.model
 
 import android.graphics.drawable.Drawable
-
+//https://www.freecodecamp.org/news/how-to-bind-data-in-android/
 data class ButtonData(val buttonText: String, val buttonImageSrc : Drawable)
